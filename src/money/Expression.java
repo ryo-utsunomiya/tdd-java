281e1206372136e7e3d4dc3def726ca9638422f9
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-15 00:17:14 +0900

    Calculate currency

--- src/money/Expression.java
+++ src/money/Expression.java
@@ -2,4 +2,6 @@ package money;
 
 interface Expression {
     Money reduce(Bank bank, String to);
+
+    Expression plus(Expression tenFrancs);
 }
--- src/money/Money.java
+++ src/money/Money.java
@@ -25,11 +25,11 @@ class Money implements Expression {
                 && currency().equals(money.currency());
     }
 
-    Money times(int multiplier) {
+    Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
-    Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
