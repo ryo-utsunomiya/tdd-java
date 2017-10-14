bea293deaf402cf28685b9396eb9165b4eaffab2
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-14 22:35:49 +0900

    Add factory method

--- src/money/Money.java
+++ src/money/Money.java
@@ -3,6 +3,8 @@ package money;
 abstract class Money {
     int amount;
 
+    abstract Money times(int multiplier);
+
     public boolean equals(Object object) {
         if (object == null) {
             return false;
@@ -12,5 +14,13 @@ abstract class Money {
 
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -7,25 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoneyTest {
     @Test
     void testMultiplication() {
-        Dollar five = new Dollar(5);
-        assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(3));
+        Money five = Money.dollar(5);
+        assertEquals(Money.dollar(10), five.times(2));
+        assertEquals(Money.dollar(15), five.times(3));
     }
 
     @Test
     void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertFalse(new Dollar(5).equals(new Franc(6)));
-        assertFalse(new Dollar(5).equals(5));
-        assertFalse(new Dollar(5).equals(null));
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertFalse(Money.dollar(5).equals(Money.franc(6)));
+        assertFalse(Money.dollar(5).equals(5));
+        assertFalse(Money.dollar(5).equals(null));
     }
 
     @Test
     void testFrancMultiplication() {
-        Franc five = new Franc(5);
-        assertEquals(new Franc(10), five.times(2));
-        assertEquals(new Franc(15), five.times(3));
+        Money five = Money.franc(5);
+        assertEquals(Money.franc(10), five.times(2));
+        assertEquals(Money.franc(15), five.times(3));
     }
 }
