1ba7999f0beb91ee5a64cd6cec68b2bc7a8c76ce
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-10-14 23:05:02 +0900

    Remove Dollar and Franc

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -15,28 +15,15 @@ class MoneyTest {
     @Test
     void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertFalse(Money.dollar(5).equals(Money.franc(6)));
+        assertFalse(Money.dollar(5).equals(Money.franc(5)));
         assertFalse(Money.dollar(5).equals(5));
         assertFalse(Money.dollar(5).equals(null));
-    }
-
-    @Test
-    void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
     }
 }
